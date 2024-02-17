45e65dfb47711f76fb3c9b60a563ad053bd60f69
Nelson Toro <nelson.toro.flores@gmail.com>
2024-02-17 18:28:46 -0400

    Props validation using React.FunctionComponent

--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -1,11 +1,12 @@
 import { Form } from 'react-bootstrap'
 import { SUPPORTED_LANGUAGES } from '../constants'
+import { type FunctionComponent } from 'react'
 
 interface Props {
   onChange: (language: string) => void
 }
 
-export const LanguageSelector = ({ onChange }: Props) => {
+export const LanguageSelector: FunctionComponent<Props> = ({ onChange }) => {
   return (
     <Form.Select aria-label='Select a lanaguage'>
       {Object.entries(SUPPORTED_LANGUAGES).map(([languageKey, languageLiteral]) => (
